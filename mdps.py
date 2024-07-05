9585ff2c95e29bc74d0bb4322b7eb2ad60c76ca4
harshithgowda30 <cs.harshithgowda30@gmail.com>
2024-07-05 23:55:40 +0530

    Update mdps.py

--- mdps.py
+++ mdps.py
@@ -9,65 +9,67 @@ st.set_page_config(page_title="Health Assistant",
                    page_icon="🧑‍⚕️")
 
 # Load the models
+
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+
 heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 with st.sidebar:
     selected = option_menu('Multiple Disease Prediction System',
-                           ['Home', 'Diabetes Prediction', 'Heart Disease Prediction', "Parkinsons Prediction"],
+                           ['Diabetes Prediction',
+                            'Heart Disease Prediction',
+                            'Parkinsons Prediction'],
                            menu_icon='hospital-fill',
-                           icons=['house', 'activity', 'heart', 'person'],
+                           icons=['activity', 'heart', 'person'],
                            default_index=0)
 
 # Function to check if any input is empty
 def check_inputs(inputs):
     return all(inputs)
 
-# Home Page
-if selected == 'Home':
-    st.title("Welcome to the Health Assistant Web Application")
-    st.write("""
-    This Web Application is designed to help users predict the likelihood of developing certain diseases based on their input features. With the use of trained and tested machine learning models, we provide predictions for Diabetes, Heart Disease, and Parkinson's Disease.
-    """)
-    st.write("## How to Use:")
-    st.write("""
-    1. Navigate to the Main Menu(>) located in the top-left corner of the screen.
-    2. Click on the desired tab among 'Diabetes Prediction', 'Heart Disease Prediction', and 'Parkinsons Prediction' to access prediction tools for specific diseases.
-    3. Enter relevant information as requested in the input fields.
-    4. Click on the "Test Result" button to obtain predictions based on the provided data.
-    """)
-    st.write("## Disclaimer:")
-    st.write("""
-    - This Web App may not provide accurate predictions at all times. When in doubt, please enter the values again and verify the predictions.
-    - You are requested to provide your Name and Email for sending details about your test results. Rest assured, your information is safe and will be kept confidential.
-    - It is important to note that individuals with specific risk factors or concerns should consult with healthcare professionals for personalized advice and management.
-    """)
-
 # Diabetes Prediction Page
 if selected == 'Diabetes Prediction':
+
+    # page title
     st.title('Diabetes Prediction using ML')
+
+    # getting the input data from the user
     col1, col2, col3 = st.columns(3)
+
     with col1:
         Pregnancies = st.text_input('Number of Pregnancies')
+
     with col2:
         Glucose = st.text_input('Glucose Level')
+
     with col3:
         BloodPressure = st.text_input('Blood Pressure value')
+
     with col1:
         SkinThickness = st.text_input('Skin Thickness value')
+
     with col2:
         Insulin = st.text_input('Insulin Level')
+
     with col3:
         BMI = st.text_input('BMI value')
+
     with col1:
         DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+
     with col2:
         Age = st.text_input('Age of the Person')
 
+    # code for Prediction
     diab_diagnosis = ''
+
+    # creating a button for Prediction
     if st.button('Diabetes Test Result'):
-        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]
+        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+                      BMI, DiabetesPedigreeFunction, Age]
+
         if not check_inputs(user_input):
             st.error("Please enter all the values.")
         else:
@@ -81,38 +83,58 @@ if selected == 'Diabetes Prediction':
 
 # Heart Disease Prediction Page
 if selected == 'Heart Disease Prediction':
+
+    # page title
     st.title('Heart Disease Prediction using ML')
+
     col1, col2, col3 = st.columns(3)
+
     with col1:
         age = st.text_input('Age')
+
     with col2:
         sex = st.text_input('Sex')
+
     with col3:
         cp = st.text_input('Chest Pain types')
+
     with col1:
         trestbps = st.text_input('Resting Blood Pressure')
+
     with col2:
         chol = st.text_input('Serum Cholestoral in mg/dl')
+
     with col3:
         fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+
     with col1:
         restecg = st.text_input('Resting Electrocardiographic results')
+
     with col2:
         thalach = st.text_input('Maximum Heart Rate achieved')
+
     with col3:
         exang = st.text_input('Exercise Induced Angina')
+
     with col1:
         oldpeak = st.text_input('ST depression induced by exercise')
+
     with col2:
         slope = st.text_input('Slope of the peak exercise ST segment')
+
     with col3:
         ca = st.text_input('Major vessels colored by flourosopy')
+
     with col1:
         thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
 
+    # code for Prediction
     heart_diagnosis = ''
+
+    # creating a button for Prediction
     if st.button('Heart Disease Test Result'):
         user_input = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+
         if not check_inputs(user_input):
             st.error("Please enter all the values.")
         else:
@@ -126,56 +148,85 @@ if selected == 'Heart Disease Prediction':
 
 # Parkinson's Prediction Page
 if selected == "Parkinsons Prediction":
+    
+    # page title
     st.title("Parkinson's Disease Prediction using ML")
+    
     col1, col2, col3, col4, col5 = st.columns(5)  
+    
     with col1:
         fo = st.text_input('MDVP Fo(Hz)')
+        
     with col2:
         fhi = st.text_input('MDVP Fhi(Hz)')
+        
     with col3:
         flo = st.text_input('MDVP Flo(Hz)')
+        
     with col4:
         Jitter_percent = st.text_input('MDVP Jitter(%)')
+        
     with col5:
         Jitter_Abs = st.text_input('MDVP Jitter(Abs)')
+        
     with col1:
         RAP = st.text_input('MDVP RAP')
+        
     with col2:
         PPQ = st.text_input('MDVP PPQ')
+        
     with col3:
         DDP = st.text_input('Jitter DDP')
+        
     with col4:
         Shimmer = st.text_input('MDVP Shimmer')
+        
     with col5:
         Shimmer_dB = st.text_input('MDVP Shimmer(dB)')
+        
     with col1:
         APQ3 = st.text_input('Shimmer APQ3')
+        
     with col2:
         APQ5 = st.text_input('Shimmer APQ5')
+        
     with col3:
         APQ = st.text_input('MDVP APQ')
+        
     with col4:
         DDA = st.text_input('Shimmer DDA')
+        
     with col5:
         NHR = st.text_input('NHR')
+        
     with col1:
         HNR = st.text_input('HNR')
+        
     with col2:
         RPDE = st.text_input('RPDE')
+        
     with col3:
         DFA = st.text_input('DFA')
+        
     with col4:
         spread1 = st.text_input('spread1')
+        
     with col5:
         spread2 = st.text_input('spread2')
+        
     with col1:
         D2 = st.text_input('D2')
+        
     with col2:
         PPE = st.text_input('PPE')
-    
+        
+    # code for Prediction
     parkinsons_diagnosis = ''
+    
+    # creating a button for Prediction    
     if st.button("Parkinson's Test Result"):
         user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
+        
         if not check_inputs(user_input):
             st.error("Please enter all the values.")
         else:
